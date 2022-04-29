3b607cc8ddd47a4f1b5f21e01031468b4355b597
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-29 08:15:11 -0300

    Requisito 1 - Crie uma caixa de texto com a qual quem usa pode interagir para inserir texto em cima da imagem escolhida

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Project Meme Generator!!!');
+
+const text = document.createElement('p');
+function addText(textoDigitado) {
+  const container = document.getElementById('meme-image-container');
+  text.id = 'meme-text';
+
+  text.innerText = textoDigitado;
+
+  container.appendChild(text);
+}
+
+const textInput = document.getElementById('text-input');
+textInput.addEventListener('keyup', (event) => {
+  addText(event.target.value);
+});
