6d88ab6285c8d706e87197df1450d44a6dde1c92
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-29 10:14:58 -0300

    Requisito 2 - O site deve permitir que quem usa faça upload de uma imagem de seu computador

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 console.log('Project Meme Generator!!!');
 
 const text = document.createElement('p');
+
 function addText(textoDigitado) {
-  const container = document.getElementById('meme-image-container');
+  const memeImageContainer = document.getElementById('meme-image-container');
   text.id = 'meme-text';
+  text.className = 'text';
 
   text.innerText = textoDigitado;
 
-  container.appendChild(text);
+  memeImageContainer.appendChild(text);
 }
 
 const textInput = document.getElementById('text-input');
 textInput.addEventListener('keyup', (event) => {
   addText(event.target.value);
+});
+
+function addImage() {
+  const memeInsert = document.getElementById('meme-insert');
+  const memeImageContainer = document.getElementById('meme-image-container');
+  const memeImage = document.getElementById('meme-image');
+
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    memeImage.src = reader.result;
+  };
+  reader.readAsDataURL(memeInsert.files[0]);
+
+  memeImageContainer.appendChild(memeImage);
+}
+
+const btnAddImage = document.getElementById('add-image');
+btnAddImage.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  addImage();
 });
